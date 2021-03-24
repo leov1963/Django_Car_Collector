b7152943f76c5360b743911e299b39a8d40f4e53
leov1963 <leov1963@gmail.com>
2021-03-24 09:56:57 -0700

    add car index view

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import models
 
 # Create your views here.
 def index(request):
@@ -8,13 +9,18 @@ def index(request):
 def about(request):
     return render(request, 'about.html')
 
+def cars_index(request):
+    return render(request, 'cars/index.html', {'cars': cars})
+
 class Car:
-    def __init__(self, name, make, model, description, year):
-        self.name = name
-        self.make = make
-        self.model = model
-        self.description = description
-        self.year = year
+    class Cat
+    
+    # def __init__(self, name, make, model, description, year):
+    #     self.name = name
+    #     self.make = make
+    #     self.model = model
+    #     self.description = description
+    #     self.year = year
 
 cars = [
     Car('Foxy', 'Ford', 'fox-body mustang', 'angry little demon', 1987),
