e49041c521247af1749be6a480c121f759a5e3b1
leov1963 <leov1963@gmail.com>
2021-03-26 10:39:28 -0700

    Add class Car to models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    name = models.CharField(max_length=50)
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.name
