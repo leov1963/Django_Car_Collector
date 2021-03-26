59d768a169fdea4ce599ae31d3161a12788ab650
leov1963 <leov1963@gmail.com>
2021-03-26 10:39:04 -0700

    Update views

--- main_app/views.py
+++ main_app/views.py
@@ -12,8 +12,7 @@ def about(request):
 def cars_index(request):
     return render(request, 'cars/index.html', {'cars': cars})
 
-class Car:
-    class Cat
+
     
     # def __init__(self, name, make, model, description, year):
     #     self.name = name
@@ -22,8 +21,8 @@ class Car:
     #     self.description = description
     #     self.year = year
 
-cars = [
-    Car('Foxy', 'Ford', 'fox-body mustang', 'angry little demon', 1987),
-    Car('Little Dragon', 'Toyota', 'Celica gt-s', 'perfect 80s rust bucket', 1984),
-    Car('Hell Camino', 'Chevy', 'el camino', 'Loud and dangerous', 1973)
-]
+# cars = [
+#     Car('Foxy', 'Ford', 'fox-body mustang', 'angry little demon', 1987),
+#     Car('Little Dragon', 'Toyota', 'Celica gt-s', 'perfect 80s rust bucket', 1984),
+#     Car('Hell Camino', 'Chevy', 'el camino', 'Loud and dangerous', 1973)
+# ]
